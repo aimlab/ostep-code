c4eac3507f410f3f0caf449647d14809fc832054
aimlab <haochen@aimlab.org>
2019-09-15 16:13:10 +0800

    Update p3.c

--- cpu-api/p3.c
+++ cpu-api/p3.c
@@ -9,12 +9,10 @@ main(int argc, char *argv[])
 {
     printf("hello world (pid:%d)\n", (int) getpid());
     int rc = fork();
-    if (rc < 0) {
-        // fork failed; exit
+    if (rc < 0) { // fork failed; exit
         fprintf(stderr, "fork failed\n");
         exit(1);
-    } else if (rc == 0) {
-        // child (new process)
+    } else if (rc == 0) { // child (new process)
         printf("hello, I am child (pid:%d)\n", (int) getpid());
         char *myargs[3];
         myargs[0] = strdup("wc");   // program: "wc" (word count)
@@ -22,8 +20,7 @@ main(int argc, char *argv[])
         myargs[2] = NULL;           // marks end of array
         execvp(myargs[0], myargs);  // runs word count
         printf("this shouldn't print out");
-    } else {
-        // parent goes down this path (original process)
+    } else { // parent goes down this path (original process)
         int wc = wait(NULL);
         printf("hello, I am parent of %d (wc:%d) (pid:%d)\n",
 	       rc, wc, (int) getpid());
